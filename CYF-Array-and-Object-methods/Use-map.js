809beedb6ebc9e77298b711c03bf352b3a530a82
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-06-20 19:23:44 +0100

    use map exercise

---
+++ CYF-Array-and-Object-methods/Use-map.js
@@ -0,0 +1,21 @@
+/*
+Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+*/
+
+const repeatStr = (n, s) => s.repeat(n);
+
+// TESTS - run the following command to test this file:
+// npm run test -- JS1-Week1/String_repeat.js
+
+test('String Repeat: (3, "*") must return ***', () => {
+  expect(repeatStr(3, '*')).toBe('***');
+});
+test('String Repeat: (5, "#") must return #####', () => {
+  expect(repeatStr(5, '#')).toBe('#####');
+});
+test('String Repeat: (2, "ha ") must return ha ha', () => {
+  expect(repeatStr(2, 'ha ')).toBe('ha ha ');
+});
