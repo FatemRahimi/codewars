1a2a4aa655a84da10462c1a73f15f6d362af1e45
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-07-17 23:43:27 +0100

    over the road practics

---
+++ CYF-Array-and-Object-methods/over-road.js
@@ -0,0 +1,47 @@
+/* Over The Road
+Task
+You've just moved into a perfectly straight street with exactly n identical houses on either side of the road. Naturally, you would like to find out the house number of the people on the other side of the street. The street looks something like this:
+
+Street
+1|   |6
+3|   |4
+5|   |2
+Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
+
+Example (address, n --> output)
+Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+1, 3 --> 6
+3, 3 --> 4
+2, 3 --> 5
+3, 5 --> 8
+Note about errors
+If you are timing out, running out of memory, or get any kind of "error", read on. Both n and address could get upto 500 billion with over 200 random tests. If you try to store the addresses of 500 billion houses in a list then you will run out of memory and the tests will crash. This is not a kata problem so please don't post an issue. Similarly if the tests don't complete within 12 seconds then you also fail.
+
+To solve this, you need to think of a way to do the kata without making massive lists or huge for loops. Read the discourse for some inspiration :)
+
+*/
+
+const overTheRoad = (address, n) => {
+    let x; 
+    if (address % 2 === 0) {
+      x = (n * 2 - address)/2 
+      return 1 + x * 2;
+    } else {
+       x = (address - 1)/2
+      return n * 2 - x * 2
+    }
+}
+
+// TESTS - run the following command to test this file:
+// npm run test -- JS3-Week2/Over_The_Road.js
+
+test('Over The Road', () => {    
+    expect(overTheRoad(1, 3)).toBe(6);
+    expect(overTheRoad(3, 3)).toBe(4); 
+    expect(overTheRoad(2, 3)).toBe(5); 
+    expect(overTheRoad(3, 5)).toBe(8); 
+    expect(overTheRoad(7,11)).toBe(16); 
+    expect(overTheRoad(23633656673, 310027696726)).toBe(596421736780);
+});
+  
