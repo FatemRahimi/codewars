dcb8e52b91cb0daee096b0cce04f64e0298af422
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-21 18:02:43 +0100

    test the files

--- CYF-Array-and-Object-methods/find-the-odd.js
+++ CYF-Array-and-Object-methods/find-the-odd.js
@@ -20,4 +20,14 @@ const findOdd = (A) => {
       }
     }
 }
+// TESTS - run the following command to test this file:
+// npm run test -- JS3-Week2/Find_the_odd_int.js
 
+test('Find the odd int', () => {    
+    expect(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).toBe(5);
+    expect(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])).toBe(-1); 
+    expect(findOdd([20,1,1,2,2,3,3,5,5,4,20,4,5])).toBe(5); 
+    expect(findOdd([10])).toBe(10); 
+    expect(findOdd([1,1,1,1,1,1,10,1,1,1,1])).toBe(10); 
+    expect(findOdd([5,4,3,2,1,5,4,3,2,10,10])).toBe(1);
+});
