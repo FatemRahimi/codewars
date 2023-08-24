84cc84de0abc217e8e217c389d037bc63163b2f7
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-24 16:07:07 +0100

    git buzz

---
+++ CYF-Array-and-Object-methods/prefizBuzz.js
@@ -0,0 +1,14 @@
+/* This is the first step to understanding FizzBuzz.
+
+Your inputs: a positive integer, n, greater than or equal to one. n is provided, you have NO CONTROL over its value.
+
+Your expected output is an array of positive integers from 1 to n (inclusive).
+
+Your job is to write an algorithm that gets you from the input to the output.
+*/
+
+const preFizz = (n) => [...Array(n)].map((e, i) => i + 1);
+
+// TESTS - run the following command to test this file:
+// npm run test -- CYF-Fundamentals/Pre_FizzBuzz_Workout_1.js
+
