8a400c21821825f7fc0ec400049d35f1f2f91aae
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-08-24 16:07:37 +0100

    tested

--- CYF-Array-and-Object-methods/prefizBuzz.js
+++ CYF-Array-and-Object-methods/prefizBuzz.js
@@ -12,3 +12,10 @@ const preFizz = (n) => [...Array(n)].map((e, i) => i + 1);
 // TESTS - run the following command to test this file:
 // npm run test -- CYF-Fundamentals/Pre_FizzBuzz_Workout_1.js
 
+test('Pre-FizzBuzz Workout #1', () => {
+  expect(preFizz(1)).toStrictEqual([1]);
+  expect(preFizz(2)).toStrictEqual([1, 2]);
+  expect(preFizz(3)).toStrictEqual([1, 2, 3]);
+  expect(preFizz(4)).toStrictEqual([1, 2, 3, 4]);
+  expect(preFizz(5)).toStrictEqual([1, 2, 3, 4, 5]);
+});
