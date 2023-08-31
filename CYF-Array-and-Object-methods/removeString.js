e8aa1620830943f75e4dba8bfd55e9b5ea17eafe
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2023-08-31 15:51:04 +0100

    remove string

---
+++ CYF-Array-and-Object-methods/removeString.js
@@ -0,0 +1,7 @@
+* Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+const noSpace = (x) => x.replace(/\s/g, '');
+
+// TESTS - run the following command to test this file:
