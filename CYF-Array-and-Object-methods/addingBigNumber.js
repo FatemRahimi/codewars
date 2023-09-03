4ac7e014c4d2c2bcffb5d1d1955ba07c5d734190
Fatemeh Rahimi <fa.rahimi5475@gmail.com>
2023-09-03 09:31:57 +0100

    tested

--- CYF-Array-and-Object-methods/addingBigNumber.js
+++ CYF-Array-and-Object-methods/addingBigNumber.js
@@ -21,3 +21,17 @@ function add(a, b) {
     }
     return resultDigits.join('');
   }
+
+
+  //test 
+  describe("Add two numbers", function(){
+    it("should pass basic tests", function() {
+      Test.assertEquals(add("1", "1"), "2");
+      Test.assertEquals(add("123", "456"), "579");
+      Test.assertEquals(add("888", "222"), "1110");
+      Test.assertEquals(add("1372", "69"), "1441");
+      Test.assertEquals(add("12", "456"), "468");
+      Test.assertEquals(add("101", "100"), "201");
+      Test.assertEquals(add('63829983432984289347293874', '90938498237058927340892374089'), "91002328220491911630239667963")
+    });
+  });
